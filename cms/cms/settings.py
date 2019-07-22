8ed38eef6712fc4bab46fcf91f3a4eb5378583c0
M-Rasekhi <m.rasekhi@gmail.com>
2019-07-22 15:36:02 +1000

    Update settings.py

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -81,7 +81,9 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(),
+'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     
 }
 # {
